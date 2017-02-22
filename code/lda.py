e769b3fa8abe46ecd16a60d5d41c006fc651fc04
Dan Saunders <djsaunde@umass.edu>
2017-02-22 12:55:28 -0500

    Fixing a typo.

--- code/lda.py
+++ code/lda.py
@@ -6,13 +6,13 @@ Template for running LDA on job description corpus.
 
 from sklearn.decomposition import LatentDirichletAllocation
 
-def print_top_words(model, feature_names, n):
+def print_top_words(model, feature_names, n_words):
     '''
-    Prints top 'n' words from the topics 'feature_names'.
+    Prints top 'n_words' words from the topics 'feature_names'.
     '''
     for topic_idx, topic in enumerate(model.components_):
         print 'Topic #%d:' % topic_idx
-        print ' '.join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
+        print ' '.join([feature_names[i] for i in topic.argsort()[:-n_words - 1:-1]])
     print '\n'
 
 # number of words to view from each topic
