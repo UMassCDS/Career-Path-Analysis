86b90dd860db77cb36075178715815666667e237
djsaunde <djsaunde@umass.edu>
2017-04-17 20:25:39 -0400

    speeding up file reading with joblib.

--- code/title.py
+++ code/title.py
@@ -15,12 +15,11 @@ from joblib import Parallel, delayed
 from hmmlearn.hmm import MultinomialHMM
 
 
-def get_title_sequence_data():
+def get_single_file_data(file, file_index, num_files):
 	'''
-	Reads the dataset of title sequences off disk, or creates it if it doesn't yet exist.
+	Reads in a single file of resume data (contains 50,000 resumes).
 	'''
-	# get a list of the files we are looking to parse for title sequences
-	files = [ file for file in os.listdir('../data/') if 'resumes.xml' in file ]
+	print '...Loading resume file', file_index, '/', num_files
 
 	# create data structure to store job title sequences
 	title_sequences = []
@@ -28,54 +27,63 @@ def get_title_sequence_data():
 	# for removing non-letter characters
 	regex = re.compile('[^a-zA-Z]')
 
-	# look through all files in the data directory
-	for idx, file in enumerate(files):
-		print '...Loading', file, '(',  str(idx + 1), '/', str(len(files)), ')'
+	# parse the XML tree
+	tree = ElementTree.parse('../data/' + file)
+	root = tree.getroot()
 
-		# parse the XML tree
-		tree = ElementTree.parse('../data/' + file)
-		root = tree.getroot()
+	# getting all resumes
+	all_resume_tags = root.findall('.//resume')
 
-		# getting all resumes
-		all_resume_tags = root.findall('.//resume')
+	# For each resume in each file
+	for resume_tag in all_resume_tags:
+		jobs = resume_tag.findall('.//experiencerecord')
 
-		# For each resume in each file
-		for resume_tag in all_resume_tags:
-			jobs = resume_tag.findall('.//experiencerecord')
+		# If there are more than two transitions in that resume
+		if len(jobs) >= 2:
+			job_list = []
+			start_year = 0
+			title = None
 
-			# If there are more than two transitions in that resume
-			if len(jobs) >= 2:
-				job_list = []
-				start_year = 0
-				title = None
+			# For each transition in that resume
+			for job in jobs:
+				for tag in job:
+					if tag.tag == 'Title':
+						if tag.text is not None and tag.text != '':
+							title = regex.sub('', tag.text.lower())
 
-				# For each transition in that resume
-				for job in jobs:
-					for tag in job:
-						if tag.tag == 'Title':
-							if tag.text is not None and tag.text != '':
-								title = regex.sub('', tag.text.lower())
+					if tag.tag == 'StartYear':
+						start_year = int(tag.text)
 
-						if tag.tag == 'StartYear':
-							start_year = int(tag.text)
+					# Make a tuple of the start year and description
+					year_title_tuple = (start_year, title)
 
-						# Make a tuple of the start year and description
-						year_title_tuple = (start_year, title)
+				# Append the tuple to list of transitions in that resume
+				job_list.append(year_title_tuple)
 
-					# Append the tuple to list of transitions in that resume
-					job_list.append(year_title_tuple)
+			# Sort them by year to create the sequence
+			job_list = sorted(job_list, key=itemgetter(0))
 
-				# Sort them by year to create the sequence
-				job_list = sorted(job_list, key=itemgetter(0))
+			# Append the sequence from that resume to the master list
+			if len(job_list) >= 2:
+				to_append = []
+				for job in job_list:
+					if job[1] != None and job[1] != '':
+						to_append.append(job[1])
+				if len(to_append) >= 2:
+					title_sequences.append(to_append)
 
-				# Append the sequence from that resume to the master list
-				if len(job_list) >= 2:
-					to_append = []
-					for job in job_list:
-						if job[1] != None and job[1] != '':
-							to_append.append(job[1])
-					if len(to_append) >= 2:
-						title_sequences.append(to_append)
+	return title_sequences
+
+
+
+def get_title_sequence_data():
+	'''
+	Reads the dataset of title sequences off disk, or creates it if it doesn't yet exist.
+	'''
+	# get a list of the files we are looking to parse for title sequences
+	files = [ file for file in os.listdir('../data/') if 'resumes.xml' in file ]
+
+	title_sequences = Parallel(8)((delayed(get_single_file_data)(file, idx, len(files)) for idx, file in enumerate(files)))
 
 	return title_sequences
 
@@ -95,12 +103,14 @@ if __name__ == '__main__':
 	n_components, n_iter = args.n_components, args.n_iter
 
 	# get the sequential job title data
-	print '\n...Importing sequential job title data.'
+	print '\n...Importing sequential job title data.\n'
 	if not 'sequential_title_data.p' in os.listdir('../data/'):
 		data = get_title_sequence_data()
 		p.dump(data, open('../data/sequential_title_data.p', 'wb'))
 	else:
 		data = p.load(open('../data/sequential_title_data.p', 'rb'))
+
+	print data
 
 	print '\nNumber of distinct job titles (after lowercasing and removing non-letter characters:', len(set([ datum for l in data for datum in l ]))
 
