9190605ad9a81c3a35c8beef17a3304148984d1a
Matthew J. H. Rattigan <mattratt@gmail.com>
2017-07-24 16:06:29 -0400

    ntasks-per-node=32

--- code/run_lda.sh
+++ code/run_lda.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=defq
 #SBATCH --time=0-06:00:00
 #SBATCH --mem=128000
-#SBATCH --ntasks-per-node=8
+#SBATCH --ntasks-per-node=32
 
 
 # args: data_file num_topics num_procs
@@ -20,6 +20,7 @@ source env/bin/activate
 
 cd code
 #time python lda.py /home/rattigan/careerpath/Career-Path-Analysis/data/timelines_sm.p 400 20
+echo "python lda.py $1 $2 $3"
 time python lda.py $1 $2 $3
 
 echo "goodbye"
