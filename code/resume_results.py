b22fedf2b2ae3d3dabb0a4c15ef370b00c035b5c
Matthew J. H. Rattigan <mattratt@gmail.com>
2018-03-26 13:12:38 -0400

    fix job loc

--- code/resume_results.py
+++ code/resume_results.py
@@ -1,7 +1,12 @@
 import sys
 import json
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from resume_lda import read_topic_word_distribs
+
+
+sns.set(color_codes=True)
 
 
 def get_mean_vals(infile_name, burn=0, lag=0, delim="\t"):
@@ -57,6 +62,16 @@ def print_state_descs(state_topic_file_name, topic_word_file_name):
     return state_topic_avg
 
 
+def get_state_topics(state_topic_file_name, num=10):
+    state_topic_avg = get_mean_vals(state_topic_file_name)
+    top_topics = []
+    for s in range(state_topic_avg.shape[0]):
+        count_topic_tups = sorted([(c, i) for i, c in enumerate(state_topic_avg[s].tolist())],
+                                  reverse=True)
+        top_topics.append(sorted([ i for c, i in count_topic_tups[:num] ]))
+    return top_topics
+
+
 def get_state_descs(state_topic_file_name, topic_word_file_name, num_words):
     topic_words = read_topic_word_distribs(topic_word_file_name)
     state_topic_avg = get_mean_vals(state_topic_file_name)
@@ -78,18 +93,74 @@ def get_state_descs(state_topic_file_name, topic_word_file_name, num_words):
     return state_words
 
 
-def print_top_trans(state_state_file_name, state_descs):
+def print_top_trans(state_state_file_name, state_descs, state_topics):
     state_state_avg = get_mean_vals(state_state_file_name)
 
+    # for s1 in range(state_state_avg.shape[0]):
+    #     s2_freq_tups = sorted([(s, f) for s, f in enumerate(state_state_avg[s1])],
+    #                           key=lambda x: x[1], reverse=True)
+    #
+    #     print "state", s1, state_descs[s1], "\n"
+    #     for s2, f in s2_freq_tups[:5]:
+    #         print "\t{:.2f} {}".format(f, s2), state_descs[s2], "\n"
+    #
+    #     print "\n\n"
+
+    # sns.distplot(state_state_avg.flatten())
+    # plt.show()
+
+    trans_tups = []
     for s1 in range(state_state_avg.shape[0]):
-        s2_freq_tups = sorted([(s, f) for s, f in enumerate(state_state_avg[s1])],
-                              key=lambda x: x[1], reverse=True)
+        trans_tups.extend([ (freq, s1, s2) for s2, freq in enumerate(state_state_avg[s1]) ])
+    print "got {} trans tups".format(len(trans_tups))
+    print_count = 0
+    for freq, s1, s2 in sorted(trans_tups, reverse=True):
+        if s1 != s2:
+            print "({}) {} {} => \t{} {}\t{}".format(print_count, s1, state_topics[s1],
+                                                     s2, state_topics[s2], freq)
 
-        print "state", s1, state_descs[s1], "\n"
-        for s2, f in s2_freq_tups[:5]:
-            print "\t{:.2f} {}".format(f, s2), state_descs[s2], "\n"
+            for desc_tup1, desc_tup2 in zip(state_descs[s1], state_descs[s2]):
+                print "\t{} {:30} {} {:30}".format(desc_tup1[1], desc_tup1[0],
+                                                   desc_tup2[1], desc_tup2[0])
 
-        print "\n\n"
+            print "\n"
+            print_count += 1
+        if print_count > 100:
+            break
+
+    # how many states account for x% of transitions?
+    mass_threshold = 0.5
+    state_count_tups = []  # (state, # of states that account for x% of transitions)
+
+    for s1 in range(state_state_avg.shape[0]):
+        trans_counts = state_state_avg[s1].tolist()
+        del trans_counts[s1]  # ignore self trans
+        mass_total = sum(trans_counts)
+        mass_curr = 0.0
+        mass_count = 0
+        for mass in sorted(trans_counts, reverse=True):
+            mass_curr += mass
+            mass_count += 1
+            if mass_curr / mass_total > mass_threshold:
+                break
+        state_count_tups.append((s1, mass_count))
+
+    # most mobile states:
+    print "most mobile:"
+    for state, count in sorted(state_count_tups, key=lambda x: x[1], reverse=True)[:20]:
+        print "state {}\t{}\t{} states make {} mass\n".format(state, count, state_descs[state],
+                                                            mass_threshold)
+
+    # least mobile states:
+    print "\nleast mobile:"
+    for state, count in sorted(state_count_tups, key=lambda x: x[1])[:20]:
+        print "state {}\t{}\t{} states make {} mass\n".format(state, count, state_descs[state],
+                                                                mass_threshold)
+
+
+
+
+
 
 
 
@@ -103,10 +174,15 @@ if __name__ == '__main__':
     # print_state_descs(s_t_file_name, t_w_file_name)
     state_descs = get_state_descs(s_t_file_name, t_w_file_name, 20)
 
-    state_descs_str = ["{}({:.4f})".format(w, f) for w, f in state_descs]
+    state_topics = get_state_topics(s_t_file_name, num=10)
+
+
+    # print state_descs
+
+    state_descs_str = [ [(w, float("{:.4f}".format(f))) for w, f in sd] for sd in state_descs]
 
     # for i, words in enumerate(state_descs):
     #     print i, ["{}({:.4f})".format(w, f) for w, f in words], "\n"
 
-    print_top_trans(s_s_file_name, state_descs_str)
+    print_top_trans(s_s_file_name, state_descs_str, state_topics)
 
