5c60866fe03424add43c8f47f3bec60e4e194c8e
Matthew J. H. Rattigan <mattratt@gmail.com>
2018-04-09 22:28:28 -0400

    career prob calcs

--- code/career_prob.py
+++ code/career_prob.py
@@ -1,7 +1,11 @@
+import logging
 import numpy as np
 import scipy.stats
 import resume_lda
 import resume_results
+
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s")
 
 
 TRANS_FILE = '/data/output/hmm_topics200_states500_iters10k_docs30k/trans.tsv'
@@ -23,11 +27,13 @@ def trans_prob(state, state_next, state_state_trans):
     return float(n_ij) / sum_n_ik
 
 
+logging.debug("aggreg doc states")
 doc_states = scipy.stats.mode(resume_results.get_output_vals(STATE_FILE, BURN, LAG, np.integer),
                               axis=0)
+logging.debug("aggreg state trans")
 state_state_trans = np.mean(resume_results.get_output_vals(TRANS_FILE, BURN, LAG, np.double),
                             axis=0)
-
+logging.debug("loading resumes")
 resumes = resume_lda.load_json_resumes_lda(LDA_FILE,
                                            min_len=MIN_RESUME_LEN, max_entries=MAX_ENTRIES)
 
@@ -46,7 +52,7 @@ for r, resume in enumerate(resumes):
     trans_product = 1.0
     for i in range(1, len(job_states)):
         trans_product *= trans_prob(job_states[i-1], job_states[i], state_state_trans)
-    print "{}\t{}\t{}\n".format(" => ".join(job_companies), len(job_states), trans_product)
+    logging.debug("{}\t{}\t{}\n".format(" => ".join(job_companies), len(job_states), trans_product))
 
 
 
