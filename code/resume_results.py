14b2fc8e8150497cb45257199e4220b3b86de903
Matthew J. H. Rattigan <mattratt@gmail.com>
2018-04-09 22:40:55 -0400

    career prob calcs

--- code/resume_results.py
+++ code/resume_results.py
@@ -52,12 +52,12 @@ def get_output_vals(infile_name, burn=0, lag=0, dtype=np.double, delim="\t"):
                 continue
             if iter % lag == 0:
                 ts, i, json_str = line.rstrip("\n").split(delim)
-                iters.append(json.loads(json_str))
+                iters.append(np.array(json.loads(json_str), dtype))
 
     logging.debug("got {} iters of output".format(len(iters)))
     for i in range(5):
-        logging.debug("iter {}: {}".format(i, iters[i]))
-        
+        logging.debug("iter {}: {} {}".format(i, iters[i].shape, iters[i])[:100])
+
     return np.array(iters, dtype)
 
 
