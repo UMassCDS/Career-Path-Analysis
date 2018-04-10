4543d71c49868f571e835212d843cda62d6b7b75
Matthew J. H. Rattigan <mattratt@gmail.com>
2018-04-09 23:10:10 -0400

    debug

--- code/career_prob.py
+++ code/career_prob.py
@@ -32,9 +32,13 @@ resume_results.inspect_output(STATE_FILE, shape_iter_idx=15000)
 logging.debug("aggreg doc states")
 doc_states = scipy.stats.mode(resume_results.get_output_vals(STATE_FILE, BURN, LAG, np.integer),
                               axis=0)
+logging.debug("doc states {}".format(doc_states.shape))
+
 logging.debug("aggreg state trans")
 state_state_trans = np.mean(resume_results.get_output_vals(TRANS_FILE, BURN, LAG, np.double),
                             axis=0)
+logging.debug("state trans {}".format(state_state_trans.shape))
+
 logging.debug("loading resumes")
 resumes = resume_lda.load_json_resumes_lda(LDA_FILE,
                                            min_len=MIN_RESUME_LEN, max_entries=MAX_ENTRIES)
@@ -48,6 +52,7 @@ for r, resume in enumerate(resumes):
         # logging.debug("{}".format(resume))
         res_ent, top_dis = job
         start, end, company, desc = res_ent
+        logging.debug("res {} job {}: {}".format(r, i, company))
         job_state = doc_states[job_idx]
         job_states.append(job_state)
         job_companies.append(company)
